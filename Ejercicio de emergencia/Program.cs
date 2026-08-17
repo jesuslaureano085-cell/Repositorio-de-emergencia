9d2523b6e3babd899451a82b4a87a209dfbdcb71
JESUS ALEXANDER LAUREANO MENJIVAR <jesuslaureano085@gmail.com>
2026-08-17 09:14:12 -0600

    resuardado

--- Ejercicio de emergencia/Program.cs
+++ Ejercicio de emergencia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ppp;
 
 namespace Ejercicio_de_emergencia
 {
@@ -16,7 +17,7 @@ namespace Ejercicio_de_emergencia
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new form1());
+            Application.Run(new frm());
         }
     }
 }
